380a53ace74d0182f257bc72578466902b278711
Ivan Baena <baenadev.io@gmail.com>
2020-06-26 15:42:37 -0400

    split chunks

---
+++ src/client/components/Image.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Image = () => {
+  return <img src='https://via.placeholder.com/150' />;
+};
+
+export default Image;
--- webpack.client.js
+++ webpack.client.js
@@ -8,6 +8,26 @@ const config = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  },
 };
 
 module.exports = merge(baseConfig, config);
